805a3261c6f7a3cc7ebfd0299cd40e2354efd79d
Ashutosh Chandra <ashutosh.chandra.520@gmail.com>
2025-11-03 17:50:19 +0530

    fix api document route issue

--- handlers/docs/apiDoc.go
+++ handlers/docs/apiDoc.go
@@ -3,5 +3,6 @@ package handlers
 import "github.com/gin-gonic/gin"
 
 func ServeAPIDocs(c *gin.Context) {
+	c.Header("Referrer-Policy", "strict-origin-when-cross-origin")
 	c.File("./static/index.html")
 }
